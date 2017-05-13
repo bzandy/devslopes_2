9ba5d5b624d2bdfd01b2a21787277baff0cd3c1f
Brian Zandy <bzandy@Brians-MacBook-Pro.local>
2017-05-13 18:05:32 -0400

    Initial Commit

---
+++ SaveTheWhales/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheWhales
+//
+//  Created by Brian Zandy on 5/13/17.
+//  Copyright © 2017 Brian Zandy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
